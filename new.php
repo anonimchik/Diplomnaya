3edebeb61012dd90a8d0f4f5463a6d049e8ac484
anonimchik <>
2019-02-16 19:12:02 +0300

    16.02.2019

--- new.php
+++ new.php
@@ -4,10 +4,18 @@ class Team
     var $name;
     var $place;
     var $prize; 
+    var $description;
+    var $wonTournaments;
+    var $logo;
+    var $appearenceDate;
+    var $site;
+    var $achievement;
 }
 $team=new Team;
+$startTime=microtime(true);
 $teamName=""; //переменная для хранения информации о названии команды
 $ref=""; //переменная для хранения ссылки конкретной команды
+$teamPage="";
 $teamRef='https://www.cybersport.ru';
     include_once('libraries/curl_query.php');
     include_once('libraries/simplehtmldom_1_7/simple_html_dom.php');
@@ -15,16 +23,27 @@ $teamRef='https://www.cybersport.ru';
     $dom=str_get_html($html);
     foreach ($dom->find('#active tr') as $teamTable){
         if($teamTable->children(1)->plaintext!="Команда"){
-            $teamName=$teamTable->children(1); //название команды
             $ref=substr($teamTable->children(1)->innertext, strpos($teamTable->children(1)->innertext, '"')+1, strpos($teamTable->children(1)->innertext, '" ')-2-strpos($teamTable->children(1)->innertext, '="')); //ссылка на страницу команды
-            //echo substr($teamTable->children(1)->innertext, strrpos($teamTable->children(1)->innertext, "https"), strrpos($teamTable->children(1)->innertext, '")')-strrpos($teamTable->children(1)->innertext, "https"))."<br>"; //ссылка на лого команды
         }
         if($teamTable->children(3)->plaintext!="Сумма призовых"){$team->prize[]=$teamTable->children(3);} //сумма призовых
         if($ref!=null)
         {
             $html=curl_get($teamRef.$ref);
             $dom=str_get_html($html);
-            echo $teamRef.$ref."<br>";
+        }
+        foreach ($dom->find('.page--team') as $page) {
+            $team->logo[]=substr($page->children(1)->children(0)->children(0)->innertext, strpos($page->children(1)->children(0)->children(0)->innertext, '"')+1, strpos($page->children(1)->children(0)->children(0)->innertext, '" ')-1-strpos($page->children(1)->children(0)->children(0)->innertext, '"')); //ссылка на лого команды
+            $team->name[]=$teamName=$page->children(1)->children(0)->children(1)->children(0)->children(0)->plaintext; //название команды
+            $team->appearenceDate[]=$page->children(1)->children(0)->children(1)->children(0)->children(1)->plaintext; //дата появления команды
+            $team->site[]=substr($page->children(1)->children(0)->children(1)->children(0)->children(2)->plaintext, strpos($page->children(1)->children(0)->children(1)->children(0)->children(2)->plaintext, " ")); //сайт команды
+            $team->prize[]=substr($page->children(1)->children(0)->children(1)->children(1)->plaintext, strpos($page->children(1)->children(0)->children(1)->children(1)->plaintext, " ")+1); //призовые команды
+            $team->description[]=$page->children(1)->children(2)->children(1)->children(0)->plaintext; //описание команды
+            $team->achievement[]=$page->children(1)->children(2)->children(1)->children(1)->plaintext; //достижения команды
+            foreach ($dom->find('.gamers') as $players) {
+                $players->children(1)->children(0)->outertext="";
+                echo $players;
+            }
         }
     }
+    echo number_format((microtime(true)-$startTime)/60, 2, ":" ,"");
 ?>
