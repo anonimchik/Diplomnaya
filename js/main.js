3d9cb79df6da4775e449d24a0d10fcd54c1d6a26
Home <https://github.com/anonimchik/test.git>
2019-03-26 12:07:09 +0300

    26.03

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    /*$(".news-image").hover(function () {
+            $(".news-title").attr("class", "news-title-hover");
+        }, function () {
+            $(".news-title-hover").attr("class", "news-title");
+        }
+    );*/
+    alert($(".news-title").hover());
+});
