f1f83b66d38f4f28db7abedd0ad95a1688792f56
Home <https://github.com/anonimchik/test.git>
2019-04-25 13:07:53 +0300

    25.04

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
 $(function () {
-    $(".owl-carousel").owlCarousel( //установка параметров слайдера 
-        {
-            autoplay: true,
-            loop: true,
-            autoplayTimeout: 3000,
-            autoplayHoverPause: true,
-            nav: true, 
-            dotData: true,
-            dotsEach: true,
-            dots: true,
-            navText: ['<i class="fas fa-chevron-circle-left"></i>','<i class="fas fa-chevron-circle-right"></i>']
-        }
-    );
-    $(".owl-stage-outer").css("border", "1px solid #666666");
-    $(".owl-prev").addClass("owl-prev");
-    $(".owl-next").addClass("owl-next");
     $('ul.tabs li a').click(function(e){
         e.preventDefault();
         var tab_id = $(this).parent().attr('data-tab');    
@@ -23,6 +7,7 @@ $(function () {
         $(this).parent().addClass('current');
         $("#"+tab_id).addClass('current');
     });
+    
     $(".match-href").hover(function () {
             $(".fas.fa-video").css("color", "rgb(93, 255, 168)");  
             $(".match-href").css("cursor", "pointer");       
@@ -30,15 +15,18 @@ $(function () {
             $(".fas.fa-video").css("color", "white"); 
         }
     );
+
     $(".vs").hover(function () {
             $(".vs").css("color", "rgb(193, 241, 135)");    
         }, function () {
             $(".vs").css("color", "white");    
         }
     );
+
     $(".match-href").click(function () { 
        location.href="#"; //ссылка на страницу матча
     });
+
     $(".news-image").hover(function () {
             $($(this)).css({
                     "width": "100%",
@@ -63,22 +51,53 @@ $(function () {
         $(this).parent().children(".news-title").children(".title-text").css({"color":"inherit"});
         }
     );
-    $(".enter-invisible").hide();
-    $(".registration-invisible").hide();
+
     $("a.enter").click(function (e) { 
         e.preventDefault();
+        if($(".registration-invisible").is(":visible"))
+        {
+            $(".registration-invisible").hide();
+        }
         $(".enter-invisible").show();
     });
+
     $("i.fa-times").click(function (e) { 
         e.preventDefault();
         $(".enter-invisible").hide();
     });
+
     $("a.registration").click(function (e) { 
         e.preventDefault();
+        if($(".enter-invisible").is(":visible"))
+        {
+            $(".enter-invisible").hide();
+        }
         $(".registration-invisible").show();
     });
+
     $("i.fa-times").click(function (e) { 
         e.preventDefault();
         $(".registration-invisible").hide();
     });
+
+    if($("div").is(".owl-carousel"))
+    {
+        $(".owl-carousel").owlCarousel( //установка параметров слайдера 
+            {
+                autoplay: true,
+                loop: true,
+                autoplayTimeout: 3000,
+                autoplayHoverPause: true,
+                nav: true, 
+                dotData: true,
+                dotsEach: true,
+                dots: true,
+                navText: ['<i class="fas fa-chevron-circle-left"></i>','<i class="fas fa-chevron-circle-right"></i>']
+            }
+        );
+        $(".owl-stage-outer").css("border", "1px solid #666666");
+        $(".owl-prev").addClass("owl-prev");
+        $(".owl-next").addClass("owl-next");
+    }
+
 });
